3be3dedfa4bfea08416c11653e0170d34e317d91
Aaron Spring <aaronspring@users.noreply.github.com>
2021-07-26 17:46:43 +0200

    Update bias_correction.py

--- bias_correction.py
+++ bias_correction.py
@@ -218,8 +218,8 @@ class XBiasCorrection(object):
         aa = self.mod_data
         if isinstance(aa, xr.Dataset):
             dtype = aa[list(aa.data_vars)[0]].dtype
-            print('No `dtype` chosen. Input is Dataset. \
-            Defaults to %s' % dtype)
+            # print('No `dtype` chosen. Input is Dataset. \
+            # Defaults to %s' % dtype)
         elif isinstance(aa, xr.DataArray):
             dtype = aa.dtype
         return dtype
